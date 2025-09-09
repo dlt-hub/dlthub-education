a1b39b7a933342ec20495fd566648461b99f6be8
Alena <alena@dlthub.com>
2025-09-09 13:18:17 +0200

    add backfill

--- courses/deployment_course_2025/modal/github_pipeline_modal_backfill.py
+++ courses/deployment_course_2025/modal/github_pipeline_modal_backfill.py
@@ -1,6 +1,6 @@
 import modal
 
-app = modal.App("run-github-pipeline-parallel")
+app = modal.App("run-github-pipeline")
 dlt_image = modal.Image.debian_slim(python_version="3.10").run_commands(
     "apt-get update",
     "apt-get install -y software-properties-common",
@@ -9,33 +9,44 @@ dlt_image = modal.Image.debian_slim(python_version="3.10").run_commands(
     'pip install "dlt[bigquery]"',
 ).add_local_python_source("github_pipeline")
 
-
 # Define the pokemon source - this will be used by the DAG
 @app.function(
     image=dlt_image,
     secrets=[modal.Secret.from_name("github-api"), modal.Secret.from_name("googlecloud-secret-bigquery")],
+    schedule=modal.Period(minutes=1),
+
 )
-def run_pipeline(resource_name):
+def run_pipeline(start_date = None,
+    end_date = None,):
     import dlt
     from github_pipeline import github_source
 
     print("Starting pipeline setup...")
     pipeline = dlt.pipeline(
-        pipeline_name=f"github_pipeline_{resource_name}",
+        pipeline_name="github_pipeline",
         destination="bigquery",
-        dataset_name="github_data",
+        dataset_name="alena_github_data",
     )
     print("Pipeline created.")
 
-    load_info = pipeline.run(github_source.with_resources(resource_name))
+    if start_date and end_date:
+        # Backfilling
+        github_source.forks.apply_hints(
+            incremental=dlt.sources.incremental(
+                "created_at",
+                initial_value=start_date,
+                end_value=end_date,
+                row_order="asc"
+            )
+        )
+
+    load_info = pipeline.run(github_source)
     print("Pipeline run complete.")
-    print(load_info)
+    print("Load info:", load_info)
     return load_info
 
 
-@app.function(image=dlt_image, schedule=modal.Period(minutes=1), secrets=[modal.Secret.from_name("github-api")])
+# Only run the pipeline if this script is executed directly
+@app.local_entrypoint()
 def main():
-    from github_pipeline import github_source
-    resources = list(github_source.resources)
-
-    run_pipeline.spawn_map(resources)
+    run_pipeline.remote()
