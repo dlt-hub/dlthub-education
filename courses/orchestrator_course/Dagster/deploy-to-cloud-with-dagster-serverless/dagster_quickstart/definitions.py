c26694911c59216b34c1e655adb0577b4de95b5e
anair123 <aashishnair8925@gmail.com>
2025-11-03 11:55:25 +0100

    add linter

--- courses/orchestrator_course/Dagster/deploy-to-cloud-with-dagster-serverless/dagster_quickstart/definitions.py
+++ courses/orchestrator_course/Dagster/deploy-to-cloud-with-dagster-serverless/dagster_quickstart/definitions.py
@@ -1,29 +1,38 @@
-from .defs.assets_parallel import dagster_github_assets_parallel # change to match our asset 
-from dagster import Definitions, load_assets_from_modules, define_asset_job, ScheduleDefinition, multiprocess_executor # import other items for scheduling
+from dagster import (
+    Definitions,
+    ScheduleDefinition,
+    define_asset_job,
+    multiprocess_executor,
+)  # import other items for scheduling
 from dagster_embedded_elt.dlt import DagsterDltResource
 
+from .defs.assets_parallel import (
+    dagster_github_assets_parallel,
+)  # change to match our asset
 
-# create a job 
+# create a job
 job = define_asset_job(
     name="github_job",
-    selection=dagster_github_assets_parallel, # must be in the form of a list
-    executor_def=multiprocess_executor.configured({"max_concurrent": 5}), # set to 5 since we have 5 endpoints
+    selection=dagster_github_assets_parallel,  # must be in the form of a list
+    executor_def=multiprocess_executor.configured(
+        {"max_concurrent": 5}
+    ),  # set to 5 since we have 5 endpoints
 )
 
 # create a schedule
 schedule = ScheduleDefinition(
     job_name="github_job",
-    cron_schedule="0 0 * * *", # run everyday,
-    name="github_daily_schedule", # parameter name is just "name"
+    cron_schedule="0 0 * * *",  # run everyday,
+    name="github_daily_schedule",  # parameter name is just "name"
 )
 
 
 # modify the definitions to include the job and the schedule
 defs = Definitions(
-    jobs= [job], # parameter name is "jobs"
+    jobs=[job],  # parameter name is "jobs"
     assets=dagster_github_assets_parallel,
     resources={
-    "dlt": DagsterDltResource(),
+        "dlt": DagsterDltResource(),
     },
-    schedules=[schedule]
+    schedules=[schedule],
 )
--- courses/orchestrator_course/Dagster/deploy-to-cloud-with-dagster-serverless/dagster_quickstart/defs/assets.py
+++ courses/orchestrator_course/Dagster/deploy-to-cloud-with-dagster-serverless/dagster_quickstart/defs/assets.py
@@ -1,8 +1,10 @@
+import os
+
 import dlt
 from dagster import AssetExecutionContext
 from dagster_embedded_elt.dlt import DagsterDltResource, dlt_assets
-from .dagster_pipeline import github_source # import the right source
-import os
+
+from .dagster_pipeline import github_source  # import the right source
 
 initial_value = os.getenv("ISSUES_INITIAL_VALUE")
 
@@ -18,23 +20,24 @@ github_source.issues.apply_hints(
 forks_initial_value = os.getenv("FORKS_INITIAL_VALUE", "")
 forks_end_value = os.getenv("FORKS_END_VALUE", "")
 
-if forks_initial_value and forks_end_value: # only apply if both env vars are set
+if forks_initial_value and forks_end_value:  # only apply if both env vars are set
     github_source.forks.apply_hints(
         incremental=dlt.sources.incremental(
             cursor_path="updated_at",
             initial_value=forks_initial_value,
             end_value=forks_end_value,
-            row_order="asc"
+            row_order="asc",
         )
     )
 
+
 @dlt_assets(
-    dlt_source=github_source, # use the correct source 
+    dlt_source=github_source,  # use the correct source
     dlt_pipeline=dlt.pipeline(
-    pipeline_name="github_dagster_pipeline",
-    dataset_name="github_dagster_dataset",
-    destination="bigquery", # use the right destination
-    progress="log",
+        pipeline_name="github_dagster_pipeline",
+        dataset_name="github_dagster_dataset",
+        destination="bigquery",  # use the right destination
+        progress="log",
     ),
     name="github",
     group_name="github",
